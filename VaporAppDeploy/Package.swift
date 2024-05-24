294f17f726c49fbb501c5186549b29c3e5a78807
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-05-24 06:57:12 +0200

    added jazzy comments

--- VaporAppDeploy/Package.swift
+++ VaporAppDeploy/Package.swift
@@ -1,18 +1,26 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+/// The package description for the VaporAppDeploy project.
 let package = Package(
+    /// The name of the package.
     name: "VaporAppDeploy",
+    /// The platforms supported by this package.
     platforms: [
         .macOS(.v10_15)
     ],
     dependencies: [
+        /// The Swift Argument Parser package dependency.
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+        /// The YamlSwift package dependency.
         .package(url: "https://github.com/behrang/YamlSwift.git", from: "3.4.3")
     ],
     targets: [
+        /// The executable target for VaporAppDeploy.
         .executableTarget(
+            /// The name of the target.
             name: "VaporAppDeploy",
+            /// The dependencies for the target.
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Yaml", package: "YamlSwift")
