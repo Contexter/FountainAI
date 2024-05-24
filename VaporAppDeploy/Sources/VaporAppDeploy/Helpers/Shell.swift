0c0f6af16adf8c38091835fea5a808aa674bebb2
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-05-24 07:09:47 +0200

    added jazzy comments

--- VaporAppDeploy/Sources/VaporAppDeploy/Helpers/Shell.swift
+++ VaporAppDeploy/Sources/VaporAppDeploy/Helpers/Shell.swift
@@ -1,5 +1,12 @@
 import Foundation
 
+/// Runs a shell command with the specified arguments and optional working directory.
+///
+/// - Parameters:
+///   - command: The command to execute.
+///   - arguments: The arguments to pass to the command.
+///   - workingDirectory: The working directory to execute the command in. Defaults to `nil`.
+/// - Throws: A fatal error if the command execution fails.
 func runShellCommand(_ command: String, arguments: [String], workingDirectory: String? = nil) {
     let process = Process()
     process.executableURL = URL(fileURLWithPath: command)
