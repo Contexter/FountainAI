fbbede7b73ad89d48154086a0cc206926266e44e
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-06-20 16:20:19 +0200

    fix: applied 2_patch.sh, created 3_patch.sh

--- MusicSound/MusicSoundOrchestrationAPI/2_patch.sh
+++ MusicSound/MusicSoundOrchestrationAPI/2_patch.sh
@@ -1,3 +1,40 @@
+#!/bin/bash
+
+# Update Package.swift to include MIDIKit
+cat <<EOL > Package.swift
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MusicSoundOrchestrationAPI",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/orchetect/MIDIKit.git", from: "0.9.6")
+    ],
+    targets: [
+        .target(name: "App", dependencies: [
+            .product(name: "Fluent", package: "fluent"),
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "Leaf", package: "leaf"),
+            .product(name: "MIDIKit", package: "MIDIKit")
+        ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
+EOL
+
+# Update GenerateMIDIFileController.swift to use MIDIKit correctly
+cat <<EOL > Sources/App/Controllers/GenerateMIDIFileController.swift
 import Vapor
 import MIDIKit
 
@@ -40,3 +77,6 @@ func generateMIDIFile(_ req: Request) throws -> EventLoopFuture<Response> {
     try midi.write(to: URL(fileURLWithPath: midiFilePath))
     return req.eventLoop.makeSucceededFuture(Response(status: .created, body: .init(string: midiFilePath)))
 }
+EOL
+
+echo "Patches applied successfully!"
--- MusicSound/MusicSoundOrchestrationAPI/Package.swift
+++ MusicSound/MusicSoundOrchestrationAPI/Package.swift
@@ -6,30 +6,24 @@ let package = Package(
     platforms: [
         .macOS(.v10_15)
     ],
+    products: [
+        .library(name: "App", targets: ["App"]),
+        .executable(name: "Run", targets: ["Run"])
+    ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0")
+        .package(url: "https://github.com/orchetect/MIDIKit.git", from: "0.9.6")
     ],
     targets: [
-        .target(
-            name: "App",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "Leaf", package: "leaf"),
-                .product(name: "Fluent", package: "fluent")
-            ],
-            path: "Sources/App"
-        ),
-        .target(
-            name: "Run",
-            dependencies: [.target(name: "App")],
-            path: "Sources/Run"
-        ),
-        .testTarget(
-            name: "AppTests",
-            dependencies: [.target(name: "App")],
-            path: "Tests/AppTests"
-        )
+        .target(name: "App", dependencies: [
+            .product(name: "Fluent", package: "fluent"),
+            .product(name: "Vapor", package: "vapor"),
+            .product(name: "Leaf", package: "leaf"),
+            .product(name: "MIDIKit", package: "MIDIKit")
+        ]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
