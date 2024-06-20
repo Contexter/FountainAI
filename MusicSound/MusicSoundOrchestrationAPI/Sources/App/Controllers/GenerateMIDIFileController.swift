f9ef4ce787255c22f3f9e4fad4f6500dc93a5b58
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-06-20 16:55:13 +0200

    applied 5_ptach, created 6_patch

--- MusicSound/MusicSoundOrchestrationAPI/Sources/App/Controllers/GenerateMIDIFileController.swift
+++ MusicSound/MusicSoundOrchestrationAPI/Sources/App/Controllers/GenerateMIDIFileController.swift
@@ -1,7 +1,7 @@
 import Vapor
 import MIDIKit
 
-struct GenerateMIDIFileRequest: Content {
+struct GenerateMIDIFileRequest: Content, Codable {
     let fileName: String
     let events: [MIDIEvent]
 }
@@ -9,7 +9,7 @@ struct GenerateMIDIFileRequest: Content {
 func generateMIDIFile(req: Request) throws -> EventLoopFuture<Response> {
     let params = try req.content.decode(GenerateMIDIFileRequest.self)
     var midi = MIDIFile()
-    var track = MIDIFileTrack()
+    var track = MIDIFile.Track()
 
     for event in params.events {
         switch event.type {
@@ -45,4 +45,18 @@ func generateMIDIFile(req: Request) throws -> EventLoopFuture<Response> {
     try midi.write(to: URL(fileURLWithPath: "output/\(params.fileName)"))
 
     return req.eventLoop.makeSucceededFuture(Response(status: .ok))
+}
+
+extension GenerateMIDIFileRequest {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.fileName = try container.decode(String.self, forKey: .fileName)
+        self.events = try container.decode([MIDIEvent].self, forKey: .events)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(fileName, forKey: .fileName)
+        try container.encode(events, forKey: .events)
+    }
 }
--- MusicSound/MusicSoundOrchestrationAPI/Sources/App/routes.swift
+++ MusicSound/MusicSoundOrchestrationAPI/Sources/App/routes.swift
@@ -2,6 +2,6 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.post("generate_midi_file", use: generateMIDIFile)
-    app.post("commit_file", use: commitFile)
-    app.post("push_to_github", use: pushToGitHub)
+    app.post("commit_file", use: commitFileHandler)
+    app.post("push_to_github", use: pushToGitHubHandler)
 }
