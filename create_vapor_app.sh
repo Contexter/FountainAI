7ba903f9baf3536d48baed7acb2c9bc01ec19942
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2024-07-13 19:33:53 +0200

    Update create_vapor_app.sh

--- create_vapor_app.sh
+++ create_vapor_app.sh
@@ -13,8 +13,15 @@ fi
 # Create a new Vapor project interactively
 vapor new $APP_NAME
 
+# Remove the .git directory to prevent it from being recognized as a submodule
+rm -rf $APP_NAME/.git
+
 # Indicate that the Vapor app was created
 echo "Vapor application $APP_NAME created successfully."
 
 # Return to the root directory
 cd ..
+
+# Refresh Git to recognize new files and directories
+git add .
+git commit -m "Add newly created Vapor application"
